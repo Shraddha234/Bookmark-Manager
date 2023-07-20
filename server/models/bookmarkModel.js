017252e189f3b37f54c7e9e772df8c1235111800
shraddha.gupta <shraddha.gupta@acsicorp.com>
2023-07-20 17:59:26 +0530

    Changes done as per front requirement

--- server/models/bookmarkModel.js
+++ server/models/bookmarkModel.js
@@ -1,20 +1,25 @@
 const mongoose = require('mongoose');
+const Category = require("../models/categoryModel");
+const mongooseAutopopulate = require('mongoose-autopopulate')
 const bookmarkSchema = new mongoose.Schema({
-    linkName: {
-        type: String,
-        required: true
-    },
-    name: {
-        type: String,
-        required: true
-    },
+    // _id: { type: mongoose.Schema.Types.ObjectId, required: true },
+    name: { type: String, required: true },
+    url: { type: String },
     note: {
         type: String
     },
     category: {
         type: mongoose.Schema.Types.ObjectId,
-        ref: 'Category',
+        ref: Category,
         required: true
+    },
+    isActive: {
+        type: Boolean,
+        default: true,
     }
 });
+
+bookmarkSchema.plugin(mongooseAutopopulate)
+
 module.exports = mongoose.model('Bookmark', bookmarkSchema);
+
