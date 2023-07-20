51f9951be8d531cdda330ec5a07cf5fa4eb307f1
shraddha.gupta <shraddha.gupta@acsicorp.com>
2023-07-20 12:50:06 +0530

    Category Model

--- server/models/categoryModel.js
+++ server/models/categoryModel.js
@@ -1,8 +1,14 @@
+//category model
+
 const mongoose = require('mongoose');
 const categorySchema = new mongoose.Schema({
     name: {
         type: String,
         required: true
+    },
+    position: {
+        type: Number,
+        required: true,
     }
 });
-module.exports = mongoose.model('Category', categorySchema);
+module.exports = mongoose.model('Category', categorySchema); 
