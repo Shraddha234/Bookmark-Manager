fec85a04b045ae92a2acc7fd9b6c4d21df8978f5
shraddha.gupta <shraddha.gupta@acsicorp.com>
2023-07-20 17:59:02 +0530

    Changes done as per front requirement

--- server/controllers/categoryController.js
+++ server/controllers/categoryController.js
@@ -1,29 +1,38 @@
 const Category = require('../models/categoryModel');
+const Bookmark = require('../models/bookmarkModel')
+
 exports.createCategory = async (req, res) => {
     try {
-        const { name, position } = req.body;
-        const category = new Category({ name, position });
-        await category.save();
-        res.json(category);
+      const { name } = req.body;
+      const category = await Category.create({ name });
+      res.status(201).json(category);
     } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: 'Server Error' });
+      res.status(500).json({ error: 'Failed to create category' });
     }
-};
+  };
+exports.getAllCategories  = async (req, res) => {
+    try {
+      if (typeof req.query.keyword === 'string') {  
+        const keyword = String(req.query.keyword); 
+        const categories = await Category.find({ name: { $regex: keyword, $options: 'i' }, isActive: true });
+        res.json(categories);
+      } else {
+        const categories = await Category.find({ isActive: true });
+        res.json(categories);
+      }
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Server error' });
+    }
+  };
 
 // Get all categories
 
-exports.getAllCategories = async (req, res) => {
-    try {
-        const categories = await Category.find();
-        res.json(categories);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: 'Server Error' });
-    }
-};
+
 
 // Edit category
+
+  
 
 exports.editCategory = async (req, res) => {
     try {
@@ -49,7 +58,7 @@ exports.editCategory = async (req, res) => {
 exports.deleteCategory = async (req, res) => {
     try {
         const { categoryId } = req.params;
-        const category = await Category.findByIdAndRemove(categoryId);
+        const category = await Category.findByIdAndUpdate(categoryId, { isActive: false }, { new: true });
         if (!category) {
             return res.status(404).json({ error: 'Category not found' });
         }
@@ -59,36 +68,110 @@ exports.deleteCategory = async (req, res) => {
         res.status(500).json({ error: 'Server Error' });
     }
 };
+exports.deleteSelectCategory = async (req, res) => {
+  try {
+    const { categoryId }  = req.body;
+
+    if (!Array.isArray(categoryId) || categoryId.length === 0) {
+      return res.status(400).json({ error: "No category selected" });
+    }
+
+    if (categoryId.length === 1) {
+      const categoryIds = categoryId[0];
+
+      const category = await Category.findByIdAndUpdate(
+        categoryIds,
+        { isActive: false },
+        { new: true }
+      );
+
+      if (!category) {
+        return res.status(404).json({ error: "Folder not found" });
+      }
+
+      return res.json({ message: "Folder deactivated successfully" });
+    }
+
+    await Category.updateMany(
+      { _id: { $in: categoryId } },
+      { isActive: false },
+      { new: true }
+    );
+
+    res.json({ message: "Selected folders deactivated successfully" });
+  } catch (error) {
+    console.error(error);
+
+    res.status(500).json({ error: "Server Error" });
+  }
+};
 
 // Move category to another category
 
-exports.moveCategory = async (req, res) => {
-    try {
-        const { categoryId, targetCategoryId } = req.body;
-        // Get the category to move
-        const category = await Category.findById(categoryId);
-        if (!category) {
-            return res.status(404).json({ error: 'Category not found' });
-        }
-        // Get the target category
-        const targetCategory = await Category.findById(targetCategoryId);
-        if (!targetCategory) {
-            return res.status(404).json({ error: 'Target category not found' });
-        }
-        // Swap the positions of the two categories
-        const tempPosition = category.position;
-        category.position = targetCategory.position;
-        targetCategory.position = tempPosition;
-        // Save the changes
-        await Promise.all([category.save(), targetCategory.save()]);
-        // await category.save();
-        // await targetCategory.save();
-        res.json({
-            message: 'Category moved successfully',
-            category: category
-        });
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: 'Server Error' });
+// exports.moveCategory = async (req, res) => {
+//     try {
+//         const { categoryId, targetCategoryId } = req.body;
+//         // Get the category to move
+//         const category = await Category.findById(categoryId);
+//         if (!category) {
+//             return res.status(404).json({ error: 'Category not found' });
+//         }
+//         // Get the target category
+//         const targetCategory = await Category.findById(targetCategoryId);
+//         if (!targetCategory) {
+//             return res.status(404).json({ error: 'Target category not found' });
+//         }
+//         // Swap the positions of the two categories
+//         const tempPosition = category.position;
+//         category.position = targetCategory.position;
+//         targetCategory.position = tempPosition;
+//         // Save the changes
+//         await Promise.all([category.save(), targetCategory.save()]);
+//         // await category.save();
+//         // await targetCategory.save();
+//         res.json({
+//             message: 'Category moved successfully',
+//             category: category
+//         });
+//     } catch (error) {
+//         console.error(error);
+//         res.status(500).json({ error: 'Server Error' });
+//     }
+// };
+exports.moveCategory  = async (req, res) => {
+  try {
+    const { selectedLinks, targetCategoryId } = req.body;
+
+    if (!Array.isArray(selectedLinks) || selectedLinks.length === 0) {
+      return res.status(400).json({ error: "No links selected" });
     }
+
+    if (!targetCategoryId) {
+      return res.status(400).json({ error: "Target category ID not provided" });
+    }
+
+    const targetCategory= await Category.findById(targetCategoryId);
+
+    if (!targetCategory) {
+      return res.status(404).json({ error: "Target category not found" });
+    }
+
+    const links = await Bookmark.find({ _id: { $in: selectedLinks } });
+
+    if (!links || links.length === 0) {
+      return res.status(404).json({ error: "Selected links not found" });
+    }
+
+    for (const link of links) {
+      link.category = targetCategoryId;
+
+      await link.save();
+    }
+
+    res.json({ message: "Selected links moved successfully" });
+  } catch (error) {
+    console.error(error);
+
+    res.status(500).json({ error: "Server Error" });
+  }
 };
