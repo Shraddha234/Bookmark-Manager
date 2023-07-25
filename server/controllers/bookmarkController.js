c641fc9663217404b6bdd3ceebaff61212cfc844
shraddha.gupta <shraddha.gupta@acsicorp.com>
2023-07-25 15:34:20 +0530

    Imported category Model

--- server/controllers/bookmarkController.js
+++ server/controllers/bookmarkController.js
@@ -1,19 +1,16 @@
 const Bookmark = require('../models/bookmarkModel');
+const Category = require('../models/categoryModel');
 
 // Get all bookmarks
 
-exports.getAllBookmarks = async(req, res) => {
-
-        try {
-      
-            const Bookmarks = await Bookmark.find({ isActive: true });
-            res.json(Bookmarks);
-          
-        } catch (error) {
-          console.error(error);
-          res.status(500).json({ error: 'Server error' });
-        }
-      
+exports.getAllBookmarks = async (req, res) => {
+    try {
+        const Bookmarks = await Bookmark.find({ isActive: true });
+        res.json(Bookmarks);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Server error' });
+    }
 };
 
 // Create a new Bookmark
@@ -21,15 +18,20 @@ exports.getAllBookmarks = async(req, res) => {
 exports.createBookmark = async (req, res) => {
     try {
         const { name, url, note, categoryId } = req.body;
+        const category = await Category.findById(categoryId);
+        if (!category) {
+            return res.status(404).json({ error: 'Category not found' });
+        }
         const bookmark = new Bookmark({ name, url, note, category: categoryId });
         await bookmark.save();
+        category.bookmarks.push(bookmark);
+        await category.save();
         res.json(bookmark);
     } catch (error) {
         console.error(error);
         res.status(500).json({ error: 'Server Error' });
     }
 };
-
 
 //Edit a bookmark
 exports.editBookmark = async (req, res) => {
@@ -38,7 +40,7 @@ exports.editBookmark = async (req, res) => {
         const { name, url, note } = req.body;
         const bookmark = await Bookmark.findByIdAndUpdate(
             bookmarkId,
-            { name,url, note },
+            { name, url, note },
             { new: true }
         );
         if (!bookmark) {
@@ -48,6 +50,31 @@ exports.editBookmark = async (req, res) => {
     } catch (error) {
         console.error(error);
         res.status(500).json({ error: 'Server Error' });
+    }
+};
+
+//share bookmark
+exports.shareBookmark = async (req, res) => {
+    const { categoryId, bookmarkId } = req.params;
+    const { userMail, actions } = req.body;
+    try {
+        const category = await Category.findById(categoryId);
+        if (!category) {
+            return res.status(404).json({ error: 'Category not found' });
+        }
+        // Find the bookmark by its _id in the bookmarks array of the category
+        const bookmark = await Bookmark.findById(bookmarkId).populate('category');
+        if (!bookmark) {
+            return res.status(404).json({ error: 'Bookmark not found' });
+        }
+        // Update the bookmark properties
+        bookmark.userMail = userMail;
+        bookmark.actions = actions;
+        await bookmark.save();
+        res.json({ message: 'Bookmark shared successfully', bookmark });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Server error' });
     }
 };
 
@@ -74,7 +101,7 @@ exports.editBookmark = async (req, res) => {
 exports.deleteBookmark = async (req, res) => {
     try {
         const { deleteId } = req.params;
-        const bookmark = await Bookmark.findByIdAndUpdate(deleteId, {isActive: false}, {new: true});
+        const bookmark = await Bookmark.findByIdAndUpdate(deleteId, { isActive: false }, { new: true });
         if (!bookmark) {
             return res.status(404).json({ error: 'Bookmark not found' });
         }
@@ -84,3 +111,32 @@ exports.deleteBookmark = async (req, res) => {
         res.status(500).json({ error: 'Server Error' });
     }
 };
+
+//view all share data
+
+exports.viewBookmark = async (req, res) => {
+    try {
+        const { categoryId } = req.params;
+        const category = await Category.findById(categoryId).populate('bookmarks');
+        if (!category) {
+            return res.status(404).json({ error: 'Category not found' });
+        }
+
+        // Create an array to store the data for each bookmark
+        const bookmarkData = category.bookmarks.map((bookmark) => ({
+            category: category.name,
+            bookmarkName: bookmark.name,
+            actions: bookmark.actions,
+            userMail: bookmark.userMail || null,
+            username: bookmark.userMail ? extractNameFromEmail(bookmark.userMail) : null,
+        }));
+        res.json(bookmarkData);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Server error' });
+    }
+};
+function extractNameFromEmail(email) {
+    const username = email.split('@')[0];
+    return username;
+}
