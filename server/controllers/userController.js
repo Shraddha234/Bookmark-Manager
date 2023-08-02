78235861f16afeac7516118621cc99dba7f7cc9c
shraddha.gupta <shraddha.gupta@acsicorp.com>
2023-08-02 17:24:53 +0530

    server code

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,40 +1,78 @@
-// const dotenv = require('dotenv');
-// const got = require('got');
-// const fetch = require('node-fetch');
 const user = require('../models/userModel')
 const jwt = require('jsonwebtoken');
 const bycrpt = require('bcrypt');
 const axios = require('axios');
+const dotenv = require('dotenv');
+dotenv.config();
 
 // dotenv.config();
 let salt = 10
 
 // Login Module
+// exports.loginController = async (req, res) => {
+//     try {
+//       const data = await user.findOne({ email: req.body.email }); // Use email field for login
+//       if (!data) {
+//         res.status(200).send({ data: "User does not exist", status: false });
+//         return;
+//       } else {
+//         // The token will expire after 30 minutes
+//         let token = jwt.sign({ data }, process.env.SECRET_KEY, { expiresIn: '200m' });
+//         let password = data.password;
+//         let inpPassword = req.body.password;
+//         bycrpt.compare(inpPassword.toString(), password, (err, response) => {
+//           if (err) throw err;
+//           if (response) {
+//             return res.status(200).send({ data: "Successfully Logged in", status: true, token: token, expire: "20" });
+//           }
+//           return res.status(200).send({ data: "Password does not match", status: false, message: "Unsuccessful" });
+//         });
+//       }
+//     } catch (error) {
+//         console.error(error)
+//       res.status(500).send({ message: "Something went wrong", data: error, status: false });
+//     }
+//   };
 exports.loginController = async (req, res) => {
     try {
-        let data = await user.find({ name: req.body.name });
-        if (data.length === 0) {
-            res.status(200).send({ data: "user doesnot exists", status: false })
-            return;
-        }
-        else {
-            // The token will expire after 30min 
-            let token = jwt.sign({ data }, "jwt-key", { expiresIn: '30m' });
-            let password = data[0].password;
-            let inpPassword = req.body.password;
-            bycrpt.compare(inpPassword.toString(), password, (err, response) => {
-                if (err) throw err;
-                if (response) {
-                    return res.status(200).send({ data: "Successfully Login ", status: true, token: token, expire: "20" });
-                }
-                return res.status(200).send({ data: "password doesnot Match", status: false, message: "Unsuccesssful" });
-
-            })
-        }
+      const { email, password } = req.body;
+  
+      // Check if the email and password fields are provided in the request body
+      if (!email || !password) {
+        return res.status(400).json({ message: 'Email and password are required', status: false });
+      }
+  
+      // Find the user with the provided email in the database
+      const data = await user.findOne({ email });
+  
+      // If the user is not found, respond with an error
+      if (!data) {
+        return res.status(404).json({ message: 'User does not exist', status: false });
+      }
+  
+      // Compare the provided password with the hashed password in the database
+      const passwordMatch = bycrpt.compare(password, data.password);
+  
+      // If the passwords don't match, respond with an error
+      if (!passwordMatch) {
+        return res.status(401).json({ message: 'Password does not match', status: false });
+      }
+  
+      // If the login is successful, generate a JWT token
+      const token = jwt.sign({ data }, process.env.SECRET_KEY, { expiresIn: '1y' });
+  
+      // Respond with a success message along with the token
+      return res.status(200).json({
+        message: 'Successfully logged in',
+        status: true,
+        token: token,
+        expiresIn: '1y'
+      });
     } catch (error) {
-        res.status(500).send({ message: "Something went wrong", data: error, status: false });
+      console.error(error);
+      res.status(500).json({ message: 'Something went wrong', data: error, status: false });
     }
-}
+  };
 
 
 // signup Module 
@@ -45,6 +83,7 @@ exports.signUpController = async (req, res) => {
             if (err) throw err;
             let data = {
                 name: req.body.name,
+                email: req.body.email,
                 password: hash,
             }
 
@@ -97,7 +136,7 @@ exports.verifyJwt = (req, res, next) => {
         if (err) {
             return res.send({ data: "Not Authenticated", message: "user is not valid", status: false });
         }
-        req.name = decode.name;
+        req.user = decode.data;
         next();
     })
 }
@@ -105,7 +144,7 @@ exports.verifyJwt = (req, res, next) => {
 // Authentication Token 
 exports.checkTokenAuthentication = (req, res) => {
     try {
-
+        const userData = req.user;
         return res.status(200).send({ message: "user is valid", data: "Authenticated", status: true });
     }
     catch (error) {
