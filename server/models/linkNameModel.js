d559b6275e4cfbf507eb444bc7f94402339c125b
shraddha.gupta <shraddha.gupta@acsicorp.com>
2023-08-02 17:25:37 +0530

    updated linkname model

--- server/models/linkNameModel.js
+++ server/models/linkNameModel.js
@@ -1,39 +1,44 @@
 const mongoose = require('mongoose');
-const Folder = require("../models/folderModel");
-const mongooseAutopopulate = require('mongoose-autopopulate')
+const Folder = require('./folderModel');
+const User = require('./userModel');
+const mongooseAutopopulate = require('mongoose-autopopulate');
+
 const linkNameSchema = new mongoose.Schema({
-    // _id: { type: mongoose.Schema.Types.ObjectId, required: true },
-    name: { type: String, required: true },
-    url: { type: String },
-    folder: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: Folder,
-        autopopulate: true
-    },
-    isActive: {
-        type: Boolean,
-        default: true,
-    },
-    userActions: [
-        {
-          userMail: {
+  name: { type: String, required: true },
+  url: { type: String },
+  folder: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: Folder,
+    autopopulate: true,
+  },
+  isActive: {
+    type: Boolean,
+    default: true,
+  },
+  links: [{ type: mongoose.Schema.Types.ObjectId, ref: 'LinkName' }],
+  belongsToFolder: { type: Boolean, default: true },
+  sharedBy: {
+    type: String, 
+  },
+  userActions: [
+    {
+      userMail: {
+        type: String,
+      },
+      actions: {
+        type: [
+          {
             type: String,
+            enum: ['read', 'write', 'delete'],
+            default: 'read',
           },
-          actions: {
-            type: [
-              {
-                type: String,
-                enum: ['read', 'write', 'delete'],
-                default: 'read'
-              },
-            ],
-          },
-        },
-      ],
+        ],
+      },
+    },
+  ],
 });
 
-linkNameSchema.plugin(mongooseAutopopulate)
+linkNameSchema.plugin(mongooseAutopopulate);
 
 const LinkName = mongoose.model('LinkName', linkNameSchema);
 module.exports = LinkName;
-
